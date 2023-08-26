5942bfa5b9731581d2db431184f61eec8eb4172f
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-08-26 17:11:00 -0600

    ejercicio 1 terminado

--- prodAx.c
+++ prodAx.c
@@ -1,77 +1,71 @@
-//--------------------------------------------------------------
-// prodAx.c
-//--------------------------------------------------------------
-// Calcula el producto de una matriz m x n por un vector tamaño n
-//--------------------------------------------------------------
-// Auth.  JJCelada - Universidad del Valle de Guatemala
-// Date   2021-10-06
-// Vers.  1.0
-
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+void prodAx(int m, int n, double * restrict A, double * restrict x, double * restrict b);
 
- void prodAx(int m, int n, double * restrict A, double * restrict x,
-   double * restrict b);
+int main(int argc, char *argv[]) {
+    double *A, *x, *b;
+    int i, j, m, n;
 
- int main(int argc, char *argv[]) {
-   double *A,*x,*b;
-   int i, j, m, n;
+    m = 10000; // número de filas de la matriz A y elementos en el vector b
+    n = 10000; // número de columnas de la matriz A y elementos en el vector x
 
-   printf("Ingrese las dimensiones m y n de la matriz: ");
-   scanf("%d %d",&m,&n);
+    // ---- Asignación de memoria para la matriz A ----
+    if ((A = (double *)malloc(m * n * sizeof(double))) == NULL)
+        perror("memory allocation for A");
 
-   //---- Asignación de memoria para la matriz A ----
-   if ( (A=(double *)malloc(m*n*sizeof(double))) == NULL )
-    perror("memory allocation for A");
+    // ---- Asignación de memoria para el vector x ----
+    if ((x = (double *)malloc(n * sizeof(double))) == NULL)
+        perror("memory allocation for x");
 
-   //---- Asignación de memoria para el vector x ----
-   if ( (x=(double *)malloc(n*sizeof(double))) == NULL )
-    perror("memory allocation for x");
+    // ---- Asignación de memoria para el vector b ----
+    if ((b = (double *)malloc(m * sizeof(double))) == NULL)
+        perror("memory allocation for b");
 
-   //---- Asignación de memoria para el vector b ----
-   if ( (b=(double *)malloc(m*sizeof(double))) == NULL )
-    perror("memory allocation for b");
+    // Inicialización de matriz A con valores aleatorios entre 0 y 1
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            A[i * n + j] = (double)rand() / RAND_MAX;
 
-  printf("Initializing matrix A and vector x\n");
+    // Inicialización del vector x con valores aleatorios entre 0 y 1
+    for (j = 0; j < n; j++)
+        x[j] = (double)rand() / RAND_MAX;
 
-  //---- Inicialización con elementos aleatorios entre 1-7 y 1-13
-  for (j=0; j<n; j++)
-    x[j] = rand()%7+1;
+    printf("Calculando el producto Ax para m = %d n = %d\n", m, n);
 
-  for (i=0; i<m; i++)
-    for (j=0; j<n; j++)
-      A[i*n+j] = rand()%13+1;
+    clock_t start = clock(); // Marca el inicio del tiempo de ejecución
+    prodAx(m, n, A, x, b);
+    clock_t end = clock(); // Marca el final del tiempo de ejecución
 
-  printf("Calculando el producto Ax para m = %d n = %d\n",m,n);
-  (void) prodAx(m, n, A, x, b);
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-  printf("\nb: \n");
-  for(j=0; j<n; j++)
-    printf("\t%0.0f ",b[j]);
-  printf("\n\n");
+    printf("\nb:\n");
+    for (j = 0; j < n; j++)
+        printf("\t%0.6f ", b[j]);
+    printf("\n\n");
 
-  free(A);free(x);free(b);
+    printf("Tiempo de ejecución: %f segundos\n", cpu_time_used);
 
-  return(0);
- }
+    free(A);
+    free(x);
+    free(b);
+
+    return 0;
+}
 
 /* ------------------------
  * prodAx
  * ------------------------
  */
-void prodAx(int m, int n, double * restrict A, double * restrict x,
-  double * restrict b){
-
+void prodAx(int m, int n, double * restrict A, double * restrict x, double * restrict b) {
     int i, j;
 
-    for(i=0; i<m; i++){
-      b[i]=0.0;
+    for (i = 0; i < m; i++) {
+        b[i] = 0.0;
 
-
-      for(j=0; j<n; j++){
-        b[i] += A[i*n + j] * x[j];
-      }
+        for (j = 0; j < n; j++) {
+            b[i] += A[i * n + j] * x[j];
+        }
     }
-  }//----prodAx----
+} //----prodAx----
---
+++ prodAx_for.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <stdlib.h>
+
+void Ax_b(int m, int n, double* A, double* x, double* b) {
+    int i, j;
+    #pragma omp parallel for
+    for(i = 0; i < m; i++) {
+        b[i] = 0.0;  // inicialización elemento i del vec.
+        for(j = 0; j < n; j++) {
+            b[i] += A[i*n + j] * x[j]; // producto punto
+        }
+    } /* −−−Fin de parallel for−−− */
+}
+
+int main() {
+    int m = 10000; // número de filas de la matriz A y elementos en el vector b
+    int n = 10000; // número de columnas de la matriz A y elementos en el vector x
+    
+    double *A = (double *)malloc(m * n * sizeof(double)); // matriz A
+    double *x = (double *)malloc(n * sizeof(double)); // vector x
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i*n + j] = (double)rand() / RAND_MAX; // llena la matriz A con valores aleatorios entre 0 y 1
+        }
+    }
+    
+    for (int j = 0; j < n; j++) {
+        x[j] = (double)rand() / RAND_MAX; // llena el vector x con valores aleatorios entre 0 y 1
+    }
+    
+    double b[m]; // vector resultado b
+    
+    clock_t start = clock(); // Marca el inicio del tiempo de ejecución
+    
+    Ax_b(m, n, A, x, b); // llama a la función
+    
+    clock_t end = clock(); // Marca el final del tiempo de ejecución
+    
+    // Imprime algunos valores del vector resultado b para verificar
+    printf("Resultado b:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("%.6f ", b[i]);
+    }
+    printf("\n...\n");
+    
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Tiempo de ejecución: %f segundos\n", cpu_time_used);
+    
+    free(A);
+    free(x);
+    
+    return 0;
+}
